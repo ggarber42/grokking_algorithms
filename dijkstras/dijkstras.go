00eb7d8be251a2ae474f9c1e0591f7fb286dcae9
ggarber42 <ggarber@protonmail.com>
2025-04-09 18:19:22 -0300

    refactor: break into functions

--- dijkstras/dijkstras.go
+++ dijkstras/dijkstras.go
@@ -19,58 +19,67 @@ func checkIfNodeExists(fasterPath map[string]Element, nodeName string) bool {
 	return false
 }
 
-func DijkstrasSearch(start, finish WeightedGraph) (map[string]int, error) {
+func populateMapPath(mapPath *map[string]Element, start WeightedGraph) error {
 	queue := common.Queue[WeightedGraph]{}
-	mapPath := make(map[string]Element)
-
 	queue.Enqueue(start)
 	for !queue.IsEmpty() {
 		n, err := queue.Dequeue()
 		if err != nil {
-			return nil, err
+			return err
 		}
 		for weight, node := range n.Neighbours {
-			exist := checkIfNodeExists(mapPath, node.Name)
+			exist := checkIfNodeExists(*mapPath, node.Name)
 			if exist {
-				element := mapPath[node.Name]
+				element := (*mapPath)[node.Name]
 				currentWeight := element.weight
 				newWeight := n.Weight + weight
 				if currentWeight > newWeight {
 					node.Weight = newWeight
-					mapPath[node.Name] = Element{name: node.Name, weight: newWeight, parent: &n}
+					(*mapPath)[node.Name] = Element{name: node.Name, weight: newWeight, parent: &n}
 				}
 			} else {
 				newWeight := n.Weight + weight
 				node.Weight = newWeight
-				mapPath[node.Name] = Element{name: node.Name, weight: newWeight}
+				(*mapPath)[node.Name] = Element{name: node.Name, weight: newWeight}
 			}
 			queue.Enqueue(node) // adds the node with the weight of the current
 		}
 	}
+	return nil
+}
 
-	elementQueue := common.Queue[Element]{}
-	fasterPath := make(map[string]int)
+func populateFasterPath(finish WeightedGraph, mapPath map[string]Element, fasterPath *map[string]int) error {
+	queue := common.Queue[Element]{}
+	queue.Enqueue(mapPath[finish.Name])
 
-	elementQueue.Enqueue(mapPath[finish.Name])
-	for !elementQueue.IsEmpty() {
-		el, err := elementQueue.Dequeue()
+	for !queue.IsEmpty() {
+		el, err := queue.Dequeue()
 		if err != nil {
-			return nil, err
+			return err
 		}
 
-		fasterPath[el.name] = el.weight
-
+		(*fasterPath)[el.name] = el.weight
 		if el.parent != nil {
 			if nextEl, ok := mapPath[el.parent.Name]; ok {
-				elementQueue.Enqueue(nextEl)
+				queue.Enqueue(nextEl)
 			}
 		}
 
 	}
+	return nil
+}
 
-	// for k, v := range mapPath {
-	// 	fasterPath[k] = v.weight
-	// }
+func DijkstrasSearch(start, finish WeightedGraph) (map[string]int, error) {
+	mapPath := make(map[string]Element)
+	err := populateMapPath(&mapPath, start)
+	if err != nil {
+		return nil, err
+	}
 
+	fasterPath := make(map[string]int)
+	err = populateFasterPath(finish, mapPath, &fasterPath)
+	if err != nil {
+		return nil, err
+	}
 	return fasterPath, nil
 }
